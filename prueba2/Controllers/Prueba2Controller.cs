682007d708d11e63048e175fca26a9776d0182a3
rth49 <renato.pth@gmail.com>
2015-05-04 00:36:16 -0500

    controller prueba

---
+++ prueba2/Controllers/Prueba2Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prueba2.Controllers
+{
+    public class Prueba2Controller : Controller
+    {
+        //
+        // GET: /Prueba2/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
